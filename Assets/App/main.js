7053fde46428d601647ce881945da6f2d30f4cec
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-03-07 19:13:20 -0300

    atualizando arquivo js

--- Assets/App/main.js
+++ Assets/App/main.js
@@ -1,42 +1,33 @@
-async function cep() {
-    try {
-        const buscaCep = await fetch(`https://viacep.com.br/ws/06395280/json/`)
-        const buscaCepConvertido = await buscaCep.json()
+function consultaCep() {
+    const cep = document.getElementById('input-cep').value
 
-        if (buscaCepConvertido.erro) {
-            throw Error('O CEP não existe')
-        }
+    let url = `https://viacep.com.br/ws/${cep}/json/`
+    
+    fetch(url).then(function (response) {
+        response.json().then(function(data){
 
-        const CEP = buscaCepConvertido.cep
-        const logradouro = buscaCepConvertido.logradouro
-        const bairro = buscaCepConvertido.bairro
-        const localidade = buscaCepConvertido.localidade
-        const UF = buscaCepConvertido.uf
-
-        const busca = document.getElementById('botao-busca')
-        const campo = document.querySelector('.campo-de-endereco')
-
-
-        busca.addEventListener('click', () => {
-            campo.innerHTML = `<h1 id="cep">CEP: <p class="dados">${CEP}</p> </h1>
-            <h1 id="Logradouro">Logradouro: <p class="dados">${logradouro}</p> </h1>
-            <h1 id="Bairro">Bairro: <p class="dados">${bairro}</p> </h1>
-            <h1 id="Localidade">Localidade: <p class="dados">${localidade}</p> </h1>
-            <h1 id="UF">UF: <p class="dados">${UF}</p> </h1>
-
-            <div class="botao">
-                <button class="botao-busca" id="nova-busca">Nova Busca</button>
-            </div>`
+            const CEP = data.cep
+            const logradouro = data.logradouro
+            const bairro = data.bairro
+            const localidade = data.localidade
+            const UF = data.uf
+    
+            const busca = document.getElementById('botao-busca')
+            const campo = document.querySelector('.campo-de-endereco')
+    
+    
+            busca.addEventListener('click', () => {
+                campo.innerHTML = `<h1 id="cep">CEP: <p class="dados">${CEP}</p> </h1>
+                <h1 id="Logradouro">Logradouro: <p class="dados">${logradouro}</p> </h1>
+                <h1 id="Bairro">Bairro: <p class="dados">${bairro}</p> </h1>
+                <h1 id="Localidade">Localidade: <p class="dados">${localidade}</p> </h1>
+                <h1 id="UF">UF: <p class="dados">${UF}</p> </h1>
+    
+                <div class="botao">
+                    <button class="botao-busca" id="nova-busca">Nova Busca</button>
+                </div>`
+            })
+            return
         })
-
-        console.log(buscaCepConvertido)
-    }
-
-    catch (erro) {
-        console.log(erro)
-    }
-}cep()
-// const inputCep = document.getElementById('input-cep')
-// const testando = inputCep.value
-
-// console.log(testando)
+    })
+}
