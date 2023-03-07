806376fde603d6c5829ea45996a614945d0e50a7
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-03-06 22:20:06 -0300

    up

--- Assets/App/main.js
+++ Assets/App/main.js
@@ -1,14 +1,42 @@
-const teste = document.getElementById('botao-busca')
+async function cep() {
+    try {
+        const buscaCep = await fetch(`https://viacep.com.br/ws/06395280/json/`)
+        const buscaCepConvertido = await buscaCep.json()
 
-const campo = document.querySelector('.campo-de-endereco')
+        if (buscaCepConvertido.erro) {
+            throw Error('O CEP não existe')
+        }
 
-teste.addEventListener('click', () => {
-    campo.innerHTML = `<h1 id="cep">CEP: <p class="dados"> 06395-280</p> </h1>
-    <h1 id="Logradouro">Logradouro: <p class="dados"> Rua Nova Prata</p> </h1>
-    <h1 id="Bairro">Bairro: <p class="dados"> Cidade Ariston Estela Azevedo</p> </h1>
-    <h1 id="Localidade">Localidade: <p class="dados"> Carapicuíba</p> </h1>
-    <h1 id="UF">UF: <p class="dados"> SP</p> </h1>
-    <div class="botao">
-        <button class="botao-busca" id="nova-busca">Nova Busca</button>
-    </div>`
-})
+        const CEP = buscaCepConvertido.cep
+        const logradouro = buscaCepConvertido.logradouro
+        const bairro = buscaCepConvertido.bairro
+        const localidade = buscaCepConvertido.localidade
+        const UF = buscaCepConvertido.uf
+
+        const busca = document.getElementById('botao-busca')
+        const campo = document.querySelector('.campo-de-endereco')
+
+
+        busca.addEventListener('click', () => {
+            campo.innerHTML = `<h1 id="cep">CEP: <p class="dados">${CEP}</p> </h1>
+            <h1 id="Logradouro">Logradouro: <p class="dados">${logradouro}</p> </h1>
+            <h1 id="Bairro">Bairro: <p class="dados">${bairro}</p> </h1>
+            <h1 id="Localidade">Localidade: <p class="dados">${localidade}</p> </h1>
+            <h1 id="UF">UF: <p class="dados">${UF}</p> </h1>
+
+            <div class="botao">
+                <button class="botao-busca" id="nova-busca">Nova Busca</button>
+            </div>`
+        })
+
+        console.log(buscaCepConvertido)
+    }
+
+    catch (erro) {
+        console.log(erro)
+    }
+}cep()
+// const inputCep = document.getElementById('input-cep')
+// const testando = inputCep.value
+
+// console.log(testando)
